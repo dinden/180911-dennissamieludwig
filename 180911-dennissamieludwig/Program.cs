ae27746c6a6d78faa5fd832c574c77168a4c1af1
dinden <dennan_gnaget@hotmail.com>
2018-09-11 13:31:38 +0200

    Add project files.

---
+++ 180911-dennissamieludwig/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace _180911_dennissamieludwig
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
